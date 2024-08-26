04c5cd98e902585fc2a886326f14d310960bf174
Léo Cajé <99546482+leocaje@users.noreply.github.com>
2024-08-26 09:51:30 -0300

    Create funcoes_python.py

---
+++ funcoes_python.py
@@ -0,0 +1,17 @@
+def saudacao():
+  print("Olá, mundo!)
+        
+# Chamando função
+saudacao()
+
+def saudacao_personalizada(nome):
+  print("Olá, ", nome + "!")
+
+saudacao_personalizada("Léo")
+
+def soma(a, b):
+  resultado = a + b
+  return resultado 
+
+total = soma(3, 5)
+print(total)
